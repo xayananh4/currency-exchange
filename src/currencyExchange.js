dac0b28cabe59a6f857fdb3aa3193eabd29214b9
xayananh4 <xayananh4@gmail.com>
2023-02-10 15:36:43 -0800

    still working on currency that are not allow in the exchange appportion

--- src/currencyExchange.js
+++ src/currencyExchange.js
@@ -5,14 +5,16 @@ export default class currencyExchange {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${fromCurrency}`)
       .then(function (response) {     
         if (!response.ok) {
+         
           
-          console.log(response.result);
+          
           const errorMessage = `${response.status} ${response.statusText}`;
           
           console.log(response.statusText);
         
           throw new Error(errorMessage);
         } else {
+          console.log(response);
           return response.json();
         }
       })
--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ function getCurreny(fromCurrency) {
   currencyExchange.getCurreny(fromCurrency)
     .then(function (response) {
       if (response.conversion_rates) {
+        console.log("hello");
         printElements(response, fromCurrency);
-      } else {
+      }
+      else if (response.errortype === undefined) {
+        printCurrencyDoesNotExistError();
+      }  
+      else {
         printError(response);
       }
     });
@@ -18,6 +23,7 @@ function printElements(response, fromCurrency) {
   const result = document.getElementById("result");
   const toCurrency = document.getElementById("to").value;
   const amt = document.getElementById("amount").value;
+  
   let rate = response.conversion_rates[toCurrency];
   let total = rate * amt;
   result.innerHTML = `${amt} ${fromCurrency} = ${total}
@@ -30,9 +36,15 @@ function printError(error, fromCurrency) {
   -- ${error}.`;
 }
 
+function printCurrencyDoesNotExistError() {
+  document.querySelector('#showResponse').innerText = 
+  'Sorry that current does not yet exist in our app. Our team working really hard on it. Please check back!';
+
+}
+
 function handleFormSubmission() {
-  // const fromCurrency = document.getElementById("from").value;
-  getCurreny('Wakandan');
+  const fromCurrency = document.getElementById("from").value;
+  getCurreny(fromCurrency);
 }
 
 window.addEventListener("load", function () {
