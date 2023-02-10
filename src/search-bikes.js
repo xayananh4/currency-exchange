cce7c3004f993f73309e1aa6aafa9ab4e119a954
xayananh4 <xayananh4@gmail.com>
2023-02-10 08:45:12 -0800

    initial commit

--- src/search-bikes.js
+++ src/search-bikes.js
@@ -1,4 +1,4 @@
-export default class SearchBikes {  
+export default class currencyExchange {  
   static getLocation(city) {
     
     return fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity`)
